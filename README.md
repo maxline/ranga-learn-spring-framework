b617ed58750dc9e5f9d2189c8d1e97e1e12fbcff
Serhii <seleon000@gmail.com>
2024-12-16 20:59:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Udemy Ranga - learn spring framework
+## Section 9: Appendix - Introduction to Spring Framework in 10 Steps (Actually 16)
+From the course Master Microservice with Spring Boot and Spring Cloud  
+https://globallogic.udemy.com/course/microservices-with-spring-boot-and-spring-cloud/learn/lecture/32398358#content
---
+++ src/main/java/com/in28minutes/springboot/ranga_learn_spring_framework/RangaLearnSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.ranga_learn_spring_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangaLearnSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangaLearnSpringFrameworkApplication.class, args);
+	}
+
+}
